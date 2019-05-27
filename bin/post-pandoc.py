37ebaf606f8bb55e90036435f1b4d6e4778ff130
Greg Wilson <gvwilson@third-bit.com>
2019-05-27 19:39:11 -0400

    Cleaning up tables

--- bin/post-pandoc.py
+++ bin/post-pandoc.py
@@ -8,9 +8,23 @@ FIGURE_PAT = re.compile(r'<embed src="figures/(.+?)\.pdf"')
 BIB_REF_PAT = re.compile(r'<span\s+class="citation"\s+data-cites="(.+?)">.+?</span>')
 BIB_ENTRY_PAT = re.compile(r'<div\s+id="ref-(.+?)">\n<p>', re.MULTILINE)
 
+EMPTY_ROW_EVEN = '''<tr class="even">
+<td style="text-align: left;"></td>
+<td style="text-align: left;"></td>
+</tr>
+'''
+
+EMPTY_ROW_ODD = '''<tr class="odd">
+<td style="text-align: left;"></td>
+<td style="text-align: left;"></td>
+</tr>
+'''
 
 def main():
-    text = sys.stdin.read()
+    text = sys.stdin.read()\
+        .replace('<table>', '<table class="table table-striped">')\
+        .replace(EMPTY_ROW_EVEN, '')\
+        .replace(EMPTY_ROW_ODD, '')
     text = FIGURE_PAT.sub(r'<img src="figures/\1.svg"', text)
     text = BIB_REF_PAT.sub(replace_ref, text)
     text = BIB_ENTRY_PAT.sub(replace_entry, text)
